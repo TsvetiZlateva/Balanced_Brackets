43f9dbecf9925b6995df9fda2c37e1046cbc7d07
TECHNICAL\zlateva.external <TECHNICAL\zlateva.external>
2021-07-06 18:02:38 +0300

    Add project files.

---
+++ BalancedBrackets.App/Helpers.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBrackets.App
+{
+    public abstract class Helpers
+    {
+        public static bool AreThereBracketsInString(char[] input)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{' || input[i] == '(' || input[i] == '[' || input[i] == '}' || input[i] == ')' || input[i] == ']')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool AreBracketsBalanced(char[] input)
+        {
+            Stack<char> openingBrackets = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{' || input[i] == '(' || input[i] == '[')
+                {
+                    openingBrackets.Push(input[i]);
+                }
+
+                if (input[i] == '}' || input[i] == ')' || input[i] == ']')
+                {
+                    if (openingBrackets.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    else if (!ArePairsEqual(openingBrackets.Pop(), input[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (openingBrackets.Count == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool ArePairsEqual(char openingBracket, char closingBracket)
+        {
+            if (openingBracket == '(' && closingBracket == ')')
+            {
+                return true;
+            }
+            else if (openingBracket == '{' && closingBracket == '}')
+            {
+                return true;
+            }
+            else if (openingBracket == '[' && closingBracket == ']')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BalancedBrackets.App/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BalancedBrackets.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Examples:
+            //char[] input = { '{', '(', ')', '}', '[', ']' };
+            //char[] input = { '(', ')', '}', '[', ']' };
+
+            Console.Write("Please enter input string, or press '.' to EXIT: ");
+            char[] input = Console.ReadLine().ToCharArray();
+
+            while (!Helpers.AreThereBracketsInString(input))
+            {
+                Console.WriteLine("There are no brackets in the input string!");
+                Console.Write("Please enter input string, or press '.' to EXIT: ");
+                input = Console.ReadLine().ToCharArray();
+            }
+
+            while (true)
+            {
+                if (input.Length == 1 && input[0] == '.')
+                {
+                    break;
+                }
+
+                if (!Helpers.AreThereBracketsInString(input))
+                {
+                    Console.WriteLine("There are no brackets in the input string!");
+                    Console.Write("Please enter input string, or press '.' to EXIT: ");
+                    input = Console.ReadLine().ToCharArray();
+                    continue;
+                }
+
+                string result = Helpers.AreBracketsBalanced(input) ? "Balanced" : "Not Balanced";
+                Console.WriteLine(result);
+
+                Console.Write("Please enter input string, or press '.' to EXIT: ");
+                input = Console.ReadLine().ToCharArray();
+            }
+        }
+    }
+}
+
